49d166b314550bb61ec243f76e9fb2b73932840c
charles-ramos <charles@back4app.com>
2020-05-11 09:35:25 -0300

    comandos para criação dos relacionamentos entre as classes

--- Entrega-15-maio/relationMDLtoPGT.js
+++ Entrega-15-maio/relationMDLtoPGT.js
@@ -3,34 +3,32 @@ const Parse = require ('parse/node')
 Parse.initialize('a9pZ8Txk8zBJgGxg63DhXzUn5raHKHa2WjYi0Fh8', 'VCxS0OoyLyCsahzui1p9Zrm53xyq0r91DEMpqeQK')
 Parse.serverURL = 'https://parseapi.back4app.com'
 
-//Operação para copiar os dados da coluna "ID_MDL" para "cod_ml"
+//Operação para copiar os dados da coluna "COD_MDL" para "id_mdl"
 
 async function run() {
-    let Opr = Parse.Object.extend("STG_OPR_ITT")
+    let Opr = Parse.Object.extend("STG_PGT")
     let queryOpr = new Parse.Query(Opr)
 
-    queryOpr.limit(1000)
+    queryOpr.limit(20000)
 
-    queryOpr.doesNotExist("ID_MDL")
+    queryOpr.exists('COD_MDL')
     
     let results = await queryOpr.find()
     for (let i =0; i < results.length; i++ ){
         let object = results[i]
-        object.set("cod_mdl", object.get("ID_MDL"))
-        object.save()
+        object.set("id_mdl", object.get("COD_MDL"))
+        await object.save()
 
     }
 }
 
-
-//Operação para criação das relations entre STG_MDL e STG_OPR_ITT
+//Operação para criação das relations entre STG_MDL e STG_PGT
 async function run() {
-    let Operation = Parse.Object.extend("STG_OPR_ITT")
+    let Operation = Parse.Object.extend("STG_PGT")
     let queryOperation = new Parse.Query(Operation)
 
-    queryOperation.limit(1000)
-    queryOperation.exists('cod_mdl')
-
+    queryOperation.limit(20000)
+    queryOperation.exists('id_mdl')
     let results = await queryOperation.find()
     for (let i = 0; i < results.length; i++){
         let object = results[i]
@@ -43,7 +41,7 @@ async function run() {
     
         relation.add(result)
         
-        await object.save()
+        object.save()
     }
 
 }
